535dea299ae51c041c4af76de26cc736f4bd5362
Rinahairun <rinah@smartpredict.ai>
2023-08-07 22:37:57 +0300

    Change position

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState,useRef } from "react";
+import React, { useState, useRef } from "react";
 
 import { Canvas } from "@react-three/fiber";
 import Box from "@mui/material/Box";
 import StepContext from "./@core/ context/stepContext";
+
+import { OrbitControls, Environment } from "@react-three/drei";
+import bg from "./images/background.hdr";
 
 const Menu = React.lazy(() => import("./components/Menu/Menu"));
 const ThreeScene = React.lazy(() =>
@@ -11,40 +14,45 @@ const ThreeScene = React.lazy(() =>
 
 const App = () => {
   const [menuStep, setStep] = useState(-1);
-  const canvasRef = useRef();
+
   return (
-    <StepContext.Provider  value={{menuStep,setStep}}  >
-          <div style={{ height: "100vh" }}>
-      <Box
-        sx={{
-          position: "absolute",
-          zIndex: 222,
-          padding: 2,
-          height: "98%",
-        }}
-      >
-        <Menu />
-      </Box>
-      <Canvas
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          height: "100vh",
-          width: "100vw",
-        }}
-        camera={{
-          position: [0, -3, 40],
-          fov: (window.innerHeight * 65) / 935,
-          far: 1000,
-          near: 0.001,
-        }}
-        ref={canvasRef}
-      >
-        <ThreeScene/>
-      </Canvas>
-    </div>
-    </StepContext.Provider >
+    <StepContext.Provider value={{ menuStep, setStep }}>
+      <div style={{ height: "100vh" }}>
+        <Box
+          sx={{
+            position: "absolute",
+            zIndex: 222,
+            padding: 2,
+            height: "98%",
+          }}
+        >
+          <Menu />
+        </Box>
+        <Canvas
+          shadows
+          dpr={[1, 2]}
+          camera={{
+            position: [0, -3, 50],
+
+            fov: (window.innerHeight * 65) / 935,
+            far: 1000,
+            near: 0.001,
+            //  fov: 30
+          }}
+          gl={{ preserveDrawingBuffer: true }}
+        >
+          <OrbitControls
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+
+          <ThreeScene />
+          <Environment files={bg} background />
+        </Canvas>
+      </div>
+    </StepContext.Provider>
   );
 };
 
